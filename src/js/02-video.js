d0027b0966267add8a97e5ceb7c80c8579496e24
KalininVitalii <105376147+KalininVitalii@users.noreply.github.com>
2022-09-12 12:09:40 -0400

    Update 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,7 +7,13 @@ const VIDEO_PLAYER = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(time, 1000));
 
-player.setCurrentTime(localStorage.getItem(VIDEO_PLAYER));
+// player.setCurrentTime(localStorage.getItem(VIDEO_PLAYER));
+
+let savedPlayer = localStorage.getItem(VIDEO_PLAYER);
+
+if (savedPlayer) {
+  player.setCurrentTime(savedPlayer);
+}
 
 function time(data) {
   localStorage.setItem(VIDEO_PLAYER, data.seconds);
